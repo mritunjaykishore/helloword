0dc3b787a3af708edf6551ff21340025614c2b45
rahulbhardwajcse <rahulbhardwaj.cse@gmail.com>
2017-06-13 09:21:44 +0530

    commit test

--- src/helloword/HelloWorld.java
+++ src/helloword/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World test1");
+		System.out.println("Hello World");
 	}
 
 }
